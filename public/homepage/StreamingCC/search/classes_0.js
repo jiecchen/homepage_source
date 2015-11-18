a44598a14646bf4073b5d5a5dfa22de58522a762
jiecchen <jiecchen@indiana.edu>
2015-11-18 16:45:25 -0500

    update

--- public/homepage/StreamingCC/search/classes_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['countmin',['CountMin',['../classCountMin.html',1,'']]],
-  ['countmin_5fbasic',['CountMin_basic',['../classCountMin__basic.html',1,'']]]
-];
--- source/StreamingCC/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['esttotweight',['estTotWeight',['../classCountMin.html#ae4f0d164f8ee77ecbf65fc83f4d8ac71',1,'CountMin::estTotWeight()'],['../classCountMin__basic.html#a6c9d299a3a2343403b4f653f41dee72a',1,'CountMin_basic::estTotWeight()']]]
-];
